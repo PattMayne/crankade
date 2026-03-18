617e6ddd8332341816aa7182b8f3ac798d567fdf
Matt Payne <pattmayne@gmail.com>
2026-03-18 10:40:29 -0400

    request verification page full flow

--- static/js/request_verification.js
+++ static/js/request_verification.js
@@ -4,87 +4,62 @@ import * as globals from './globals.js'
 
 
 /**
- * Functions for the login input page
+ * Functions for the request verification page
  **/
 
-let err_msgs = []
 
-const submit = async () => {
-    console.log("pressing submit")
-    return
-
-    err_msgs = []
-    const pass_element = document.getElementById("password")
-    const username_or_email_element = document.getElementById("username_or_email")
-    const client_id_element = document.getElementById("client_id")
-
-    const creds = {
-        password: pass_element.value.trim(),
-        username_or_email: username_or_email_element.value.trim(),
-        client_id: client_id_element.value.trim()
-    }
-
-    // Check the inputs (identifier must match email OR username specifications)
-    // The backend will figure out which thing we did
-    let all_fields_legit =
-        utils.check_username(creds.username_or_email, err_msgs) ||
-        utils.check_email(creds.username_or_email, err_msgs)
-    all_fields_legit = utils.check_password(creds.password, err_msgs) && all_fields_legit
-
-    if (!all_fields_legit) {
-        show_err_box()
-        return
-    } else {
-        err_msgs = []
-        hide_err_box()
-    }    
-
-    // now send it to the login route
-    const route = "/auth/login"
-
-    await utils.fetch_json_post(route, creds)
-        .then(response => {
-            if (!response.ok) {
-                response.json().then(data => {
-                    let msg = (!!data.code) ? (data.code.toString() + " ") : ""
-                    msg += (!!data.error) ? data.error : " Error occurred"
-                    err_msgs.push(msg)
-                    show_err_box()
-                })
-
-                throw new Error("User not found or server error.")
-            }
-            return response.json()
-        }).then(data => {
-            console.log("data: ", data)
-            if (!!data.username){
-                window.location.href = "/dashboard";
-            } else if (!!data.redirect_uri) {
-                window.location.href = data.redirect_uri
-            }
-            
-        }).catch(error => {
-            console.log('Error: ', error)
-        })
-}
-
+let msgs = []
 
 // SHOW/HIDE ERROR BOX
+const hide_msg_box = () =>
+    document.getElementById("msg_box").classList.add('hidden')
 
-const hide_err_box = () =>
-    document.getElementById("err_msg_box").classList.add('hidden')
+const show_msg_box = () => {
+    const msg_box = document.getElementById("msg_box")
+    msg_box.innerHTML = "";
 
-const show_err_box = () => {
-    const err_box = document.getElementById("err_msg_box")
-    err_box.innerHTML = "";
-
-    for (let err_msg of err_msgs) {
-        const msg_p = "<p>" + err_msg + "</p>"
-        err_box.innerHTML += msg_p
+    for (let msg of msgs) {
+        const msg_p = "<p>" + msg + "</p>"
+        msg_box.innerHTML += msg_p
     }
 
-    err_box.classList.remove('hidden')
+    msg_box.classList.remove('hidden')
+    msgs = []
 }
+
+
+async function request_new_code() {
+    console.log("REQUESTING NEW CODE")
+    const route = "/req_new_code"
+    let email = document.getElementById("email_req").value
+    let inputs = {
+        "email": email.toString()
+    }
+
+    await utils.fetch_json_post(route, inputs)
+    .then(response => {
+        if (!response.ok) {
+            response.json().then(data => {
+                const msg = !!data.error && !!data.code ?
+                    "<h3>" + data.code + "</h3><p>" + data.error + "</p>" :
+                    "Error Occurred"
+                msgs.push(msg)
+                show_msg_box()
+            })
+
+            throw new Error("Inputs invalid or server error.")
+        }
+        return response.json()
+    }).then(update_data => {
+        if (!!update_data.message) {
+            msgs.push(update_data.message)
+            show_msg_box()
+        }        
+    }).catch(error => {
+        console.log('Error: ', error)
+    })
+}
+
 
 
 // Add event listeners
@@ -93,7 +68,7 @@ document.addEventListener('DOMContentLoaded', () => hide_err_box())
 document.getElementById('email').addEventListener(
     'keydown', (e) => (e.key === 'Enter') && submit_login())
 document.getElementById('submit_btn').addEventListener(
-    'click', () => submit())
+    'click', () => request_new_code())
 
 // Make functions available to the HTML elements (via window)
 
